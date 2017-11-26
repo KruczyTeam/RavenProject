5d90fabe797ad14b3bbee5e3ef388e6fde150f0f
Xiaoyo <grzesiekk15@gmail.com>
2017-11-26 23:03:09 +0100

    changes after problem with mysql and spring

--- Raven/raven-application/src/main/java/com/kruczyteam/raven/Main.java
+++ Raven/raven-application/src/main/java/com/kruczyteam/raven/Main.java
@@ -1,10 +1,12 @@
 package com.kruczyteam.raven;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Main
 {
 	public static void main(String[] args)
